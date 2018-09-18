af180323bd57be0d340d95f0dab9538bbed406f3
Emma Strubell <emma.strubell@gmail.com>
2018-09-18 12:09:48 -0400

    get data script

--- extract_test_from_brown.sh
+++ extract_test_from_brown.sh
@@ -6,7 +6,10 @@ SECTIONS="01 02 03"
 
 mkdir -p $CONLL05/test.brown/synt
 
-rm $CONLL05/test.brown/synt/test.brown.synt.gz
+# Remove output file if it exists, since we're appending
+if [ -e $CONLL05/test.brown/synt/test.brown.synt.gz ]; then
+    rm $CONLL05/test.brown/synt/test.brown.synt.gz
+fi
 
 for section in $SECTIONS; do
     cat $BROWN/CK/CK$section.MRG | awk '{if($1 !~ "*x*") print}' | $SRLCONLL/bin/wsj-removetraces.pl | $SRLCONLL/bin/wsj-to-se.pl -w 0 -p 1 | gzip >> $CONLL05/test.brown/synt/test.brown.synt.gz
---
+++ get_data.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+conll05_data_url="http://www.lsi.upc.edu/~srlconll/conll05st-release.tar.gz"
+conll05_scripts_url="http://www.lsi.upc.edu/~srlconll/srlconll-1.1.tgz"
+
+wget $conll05_data_url
+wget $conll05_scripts_url
+tar xzvf conll05st-release.tar.gz
+tar xzvf srlconll-1.1.tgz
+
+export SRLCONLL="`pwd`/srlconll-1.1"
+export CONLL05="`pwd`/conll05st-release"
+export PERL5LIB=$SRLCONLL/lib:$PERL5LIB
